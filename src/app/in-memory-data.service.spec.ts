a62b60f1f0ce513fb5b44f80c5dd8e794ec84428
workplace <leonhunt.313@gmail.com>
2018-12-11 11:31:31 -0800

    third commit

---
+++ src/app/in-memory-data.service.spec.ts
@@ -0,0 +1,40 @@
+// import { TestBed } from '@angular/core/testing';
+
+// import { InMemoryDataService } from './in-memory-data.service';
+
+// describe('InMemoryDataService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
+
+//   it('should be created', () => {
+//     const service: InMemoryDataService = TestBed.get(InMemoryDataService);
+//     expect(service).toBeTruthy();
+//   });
+// });
+
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
